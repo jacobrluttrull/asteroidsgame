4fdc7b72c41e77005b074c57e765dfc037607fda
jacobrluttrull <jacobrluttrull@gmail.com>
2025-12-10 23:09:57 -0500

    added cooldown timer

--- player.py
+++ player.py
@@ -1,11 +1,13 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, \
+    PLAYER_SHOOT_COOLDOWN_SECONDS
 import pygame
 from shot import Shot
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0  # in degrees
+        self.cooldown_timer = 0
 
     # in the Player class
     def triangle(self):
@@ -24,20 +26,32 @@ class Player(CircleShape):
         )
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
+
+        # tick down cooldown
+        if self.cooldown_timer > 0:
+            self.cooldown_timer -= dt
+            if self.cooldown_timer < 0:
+                self.cooldown_timer = 0  # optional
 
         if keys[pygame.K_a]:
             self.rotate(dt)
 
         if keys[pygame.K_d]:
             self.rotate(-dt)
+
         if keys[pygame.K_w]:
             self.move(dt)
+
         if keys[pygame.K_s]:
             self.move(-dt)
-        if keys[pygame.K_SPACE]:
+
+        # only shoot if cooldown is done
+        if keys[pygame.K_SPACE] and self.cooldown_timer <= 0:
             self.shoot()
+            self.cooldown_timer = PLAYER_SHOOT_COOLDOWN_SECONDS
 
     def move(self, dt):
         unit_vector = pygame.Vector2(0, 1)
@@ -49,5 +63,7 @@ class Player(CircleShape):
     def shoot(self):
         shot = Shot(self.position.x, self.position.y, self.radius)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+
+
 
 
